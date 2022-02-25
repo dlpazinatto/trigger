ea68b7fc6c10b88c113e480319ce38be72efe1bf
Dilceu <dlpazinatto@gmail.com>
2022-02-25 13:59:21 -0300

    fix date and timezone on logs

--- src/controller/DadosTriggerDb.ts
+++ src/controller/DadosTriggerDb.ts
@@ -1,23 +1,27 @@
 import moment from 'moment';
-import tz from 'moment-timezone';
 import { Request, Response } from 'express';
 import legado from './EnviaDadosRest';
 
 
+const tz = moment().utcOffset(-180);
 
 export async function baseFormula (req: Request, res: Response) {
     try{
         let id: number = req.body.id as number;
+        let id_processo = Math.floor((new Date().getTime() / 1000));
+        let chave: string = (id_processo.toString() + id.toString().padStart(10,'0'));
         
-        console.log(`${moment().utcOffset(-180).format("YYYY-MM-DD HH:mm:ss").toString()} ID: ${id}. Cliente: ${req.body.cliente as string}. Ação: ${req.body.acao as string}\n`);
-        //.tz('America/Sao_paulo')
+        let inicio = tz.format("YYYY-MM-DD HH:mm:ss").toString();
+        
+        console.log(`${inicio} ID: ${id}. Cliente: ${req.body.cliente as string}. Ação: ${req.body.acao as string} ID processo: ${chave}\n`);
         
         res.status(200);
         res.json([{
             status: 'Processado',
-            id
+            id,
+            chave
         }]);
-        await enviaDados(id);
+        await enviaDados(id, chave, inicio);
     }catch(error){
 
         res.status(200);
@@ -28,16 +32,16 @@ export async function baseFormula (req: Request, res: Response) {
     
 }
 
-async function enviaDados(id:number) {
+async function enviaDados(id:number, id_processo: string, inicio:string) {
     try {
         const resp = await legado.post('api/motor/recalc/indicator/'+id, ({
             id: id,
             status: 'processado'
         }))
-        console.log( `${moment().utcOffset(-180).format("YYYY-MM-DD HH:mm:ss").toString()} Processado ID: ${id}` );
-        //.tz('America/Sao_paulo')
+        console.log( `${tz.format("YYYY-MM-DD HH:mm:ss").toString()} Processado ID: ${id} Processo: ${id_processo} que foi iniciado as ${inicio}` );
+        
     } catch (error) {
-        console.log(`${moment().utcOffset(-180).format("YYYY-MM-DD HH:mm:ss").toString()} Erro:  ${error}`)
+        console.log(`${tz.format("YYYY-MM-DD HH:mm:ss").toString()} Erro:  ${error}`)
     }
     
 }
